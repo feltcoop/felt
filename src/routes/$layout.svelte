c6683b1507193070aed4d1a4944f6e965695b3fe
Ryan Atkinson <mail@ryanatkn.com>
2021-04-23 21:12:44 -0600

    layout

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,9 +1,30 @@
 <script>
 	import '../app.css';
+	import Nav from '$lib/Nav.svelte';
 </script>
 
 <svelte:head>
 	<link rel="icon" href="favicon.ico" />
 </svelte:head>
 
-<slot />
+<div id="nav"><Nav /></div>
+<main>
+	<slot />
+</main>
+
+<style>
+	#nav {
+		position: absolute;
+		left: 0;
+		top: 0;
+		width: 100%;
+		height: var(--nav_height);
+	}
+	main {
+		height: 100%;
+		padding-top: var(--nav_height);
+		text-align: center;
+		padding: 1em;
+		margin: 0 auto;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,31 +7,8 @@
 		.sort((a, b) => a.name.localeCompare(b.name));
 </script>
 
-<main>
-	<h1>
-		here's the <a href="https://github.com/feltcoop/felt-mockup">Felt mockup</a> we made last year. more
-		soon
-	</h1>
-
-	<Icons {icons} />
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		margin: 0 auto;
-	}
-
-	p {
-		max-width: 14rem;
-		margin: 2rem auto;
-		line-height: 1.35;
-	}
-
-	@media (min-width: 480px) {
-		p {
-			max-width: none;
-		}
-	}
-</style>
+<h1>
+	here's the <a href="https://github.com/feltcoop/felt-mockup">Felt mockup</a> we made last year. more
+	soon
+</h1>
+<Icons {icons} />
