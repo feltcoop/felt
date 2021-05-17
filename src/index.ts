de83114a70c3b21e21c6c33f843be3c7c91971f4
Ryan Atkinson <mail@ryanatkn.com>
2021-05-17 17:30:14 -0600

    add index with public interface

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// The existence of this file tells Gro that we're in a library project by default.
+// With no further config, this entrypoint will be bundled into `dist/` by the default adapter.
+
+export {default as FeltHeart} from './lib/FeltHeart.svelte';
+export {default as Icons} from './lib/Icons.svelte';
+export * as icons from './lib/icons.js';
+export {default as Nav} from './lib/Nav.svelte';
+export {default as PendingAnimation} from './lib/PendingAnimation.svelte';
