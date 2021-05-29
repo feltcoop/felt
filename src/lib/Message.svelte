b802fdda18d34dd3222311f917a3caff24297579
Ryan Atkinson <mail@ryanatkn.com>
2021-05-29 16:31:57 -0600

    extract Message component

---
+++ src/lib/Message.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	export let text: string | null;
+</script>
+
+{#if text}
+	<div class="message">
+		<div class="text">
+			{text}
+		</div>
+	</div>
+{/if}
+
+<style>
+	.message {
+		min-height: var(--message_min_height);
+		width: 100%;
+		color: var(--message_color);
+		font-weight: bold;
+		font-size: var(--font_size_md);
+		margin: var(--spacing_lg) 0;
+		border-top: 2px solid var(--message_color);
+		border-bottom: 2px solid var(--message_color);
+		display: flex;
+		align-items: stretch;
+	}
+	.text {
+		width: 100%;
+		border-top: 2px dotted var(--message_color);
+		border-bottom: 2px dotted var(--message_color);
+		padding: var(--spacing_md) 0;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		text-align: center;
+	}
+</style>
--- src/lib/Plain_Message.svelte
+++ src/lib/Plain_Message.svelte
@@ -1,27 +1,7 @@
 <script lang="ts">
+	import Message from './Message.svelte';
+
 	export let text: string | null;
 </script>
 
-<!-- TODO refactor/rethink, see others -->
-{#if text}
-	<div class="text">
-		{text}
-	</div>
-{/if}
-
-<style>
-	.text {
-		min-height: var(--message_min_height);
-		width: 100%;
-		/* TODO vars */
-		font-size: var(--font_size_md);
-		margin: var(--spacing_lg) 0;
-		padding: var(--spacing_md) 0;
-		border-top: var(--border_plain);
-		border-bottom: var(--border_plain);
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		text-align: center;
-	}
-</style>
+<Message {text} />
