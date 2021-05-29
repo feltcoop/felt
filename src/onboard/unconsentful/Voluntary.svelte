988f796eeba9acac93e72e27d145216a0550cc8f
Ryan Atkinson <mail@ryanatkn.com>
2021-05-29 13:56:49 -0600

    form stuff

--- src/onboard/unconsentful/Voluntary.svelte
+++ src/onboard/unconsentful/Voluntary.svelte
@@ -4,12 +4,16 @@
 	export let data: OnboardData;
 	export let done: () => void;
 
+	const create = (username: string, password: string) => {
+		console.log('create', username);
+		password;
+	};
+	const logInWith = (name: string) => {
+		console.log('logInWith name', name);
+	};
+
 	let username = '';
 	let password = '';
-
-	const create = () => {
-		console.log('create');
-	};
 </script>
 
 <h1>unconsentful : voluntary</h1>
@@ -17,7 +21,24 @@
 <form>
 	<input bind:value={username} placeholder="username" />
 	<input bind:value={password} placeholder="password" />
-	<button type="button" on:click={create}>create account</button>
-	<button type="button"> log in with $SOCIAL</button>
-	<button type="button"> log in with $TRACKER</button>
+	<button type="button" on:click={() => create(username, password)}>create account</button>
+
+	<div class="or">or</div>
+
+	<button type="button" on:click={() => logInWith('$SOCIAL')}> log in with $SOCIAL</button>
+	<button type="button" on:click={() => logInWith('$TRACKER')}> log in with $TRACKER</button>
+	<button type="button" on:click={() => logInWith('$TRUSTED')}> log in with $TRUSTED</button>
 </form>
+
+<style>
+	.or {
+		/* TODO vars  */
+		border-top: 2px dashed #ccc;
+		border-bottom: 2px dashed #ccc;
+		margin: 20px 0;
+		padding: 10px 0;
+		font-size: 32px;
+		width: 100%;
+		text-align: center;
+	}
+</style>
