2f58a186f4e44eb1bd45f9a16bcb14cd0565c049
Ryan Atkinson <mail@ryanatkn.com>
2021-05-29 23:28:04 -0600

    move a line

--- src/onboard/Credits.svelte
+++ src/onboard/Credits.svelte
@@ -1,8 +1,7 @@
+<h3>TODO proper credits</h3>
 <p>
 	this study is inspired by <a href="https://consentful.systems">consentful.systems</a>
 </p>
-<p>(TODO proper credits)</p>
-
 <p>
 	permissive open source code at <a href="https://github.com/feltcoop/felt">github</a> 💚
 </p>
