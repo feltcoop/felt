887d1239afd43ff5dcac6d7e168e5a4fd91f526f
Ryan Atkinson <mail@ryanatkn.com>
2021-05-30 01:23:30 -0600

    add end arrow

--- src/onboard/unconsentful/End.svelte
+++ src/onboard/unconsentful/End.svelte
@@ -4,3 +4,13 @@
 	export let data: Onboard_Data;
 	export let done: () => void;
 </script>
+
+<div>→</div>
+
+<style>
+	div {
+		font-size: var(--spacing_xxxxxl);
+		text-align: center;
+		color: var(--text_disabled_color);
+	}
+</style>
