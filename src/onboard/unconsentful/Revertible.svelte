c17fda9b412f0f57e49c94c5f680f3db33149992
Ryan Atkinson <mail@ryanatkn.com>
2021-05-29 22:52:22 -0600

    improve a line

--- src/onboard/unconsentful/Revertible.svelte
+++ src/onboard/unconsentful/Revertible.svelte
@@ -9,8 +9,8 @@
 <Content>
 	<p>Great! We'll never bother you about those things again unless our lawyers make us.</p>
 	<p>
-		Settings are so annoying right! We're burying those annoying options as deeply as we can, as we
-		speak! Software is hard, ha ha ha.
+		Settings are so annoying right! We're hard at work burying those annoying options as deep as we
+		can, because customer service comes first.
 	</p>
 	<button on:click={() => done()}>continue →</button>
 </Content>
