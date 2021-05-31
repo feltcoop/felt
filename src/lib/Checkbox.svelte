dd5dfb4ef09b7ad6011a73d6d9a4a3bae08b4232
Ryan Atkinson <mail@ryanatkn.com>
2021-05-30 23:18:32 -0600

    add buttonlike class

--- src/lib/Checkbox.svelte
+++ src/lib/Checkbox.svelte
@@ -5,7 +5,7 @@
 	$: on_change && on_change(checked);
 </script>
 
-<div class="checkbox" class:checked>
+<div class="checkbox buttonlike" class:checked>
 	<label>
 		<input type="checkbox" bind:checked />
 		<slot />
@@ -26,11 +26,6 @@
 		border-bottom: var(--border_width_md) var(--border_style)
 			var(--custom_border_color, var(--border_lighter_color));
 	}
-	.checkbox:hover {
-		z-index: 1;
-		outline: var(--spacing_rg) var(--border_accent_style)
-			var(--custom_border_color, var(--interactive_hover_color));
-	}
 	label {
 		width: var(--column_width);
 		padding: var(--spacing_md) 0;
@@ -41,7 +36,6 @@
 	.checked {
 		--custom_border_color: var(--selected_color);
 	}
-	.checkbox:active,
 	.checked:active {
 		--custom_border_color: var(--border_active_color);
 	}
