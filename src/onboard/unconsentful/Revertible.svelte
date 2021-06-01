10fc02ae1de255d509f1b87bb57efa57b6aae184
Ryan Atkinson <2608646+ryanatkn@users.noreply.github.com>
2021-06-01 09:44:15 -0600

    roll eyes
    Co-authored-by: greatbacon <4512607+greatbacon@users.noreply.github.com>

--- src/onboard/unconsentful/Revertible.svelte
+++ src/onboard/unconsentful/Revertible.svelte
@@ -12,7 +12,7 @@
 	</blockquote>
 	<p>
 		Just checking, you accept all of that stuff, right? Our lawyers are making us ask, you know how
-		lawyers are.
+		lawyers are. 🙄
 	</p>
 	<button on:click={() => done()}>I accept →</button>
 </Markup>
