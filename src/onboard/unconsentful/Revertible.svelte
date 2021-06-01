fc01c6c0e43fb689e094dfffd0ebce84f1a4ee70
Ryan Atkinson <mail@ryanatkn.com>
2021-05-31 21:05:22 -0600

    reline

--- src/onboard/unconsentful/Revertible.svelte
+++ src/onboard/unconsentful/Revertible.svelte
@@ -9,8 +9,8 @@
 <Markup>
 	<p>Great! We'll never bother you about those things again unless our lawyers make us.</p>
 	<p>
-		Settings are so annoying right! We're hard at work burying them as deep as we can, because we
-		put you the customer* first.
+		Settings are so confusing right! That's why we're hard at work burying them as deep as we can,
+		because we put the customer first.
 	</p>
 	<button on:click={() => done()}>continue →</button>
 </Markup>
