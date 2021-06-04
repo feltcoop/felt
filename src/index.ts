2108e8a47df14c04f3f08307ecd16a8329e614d3
Ryan Atkinson <mail@ryanatkn.com>
2021-06-03 23:20:14 -0600

    snakes beats camels

--- src/index.ts
+++ src/index.ts
@@ -11,21 +11,20 @@ export * from './utils/types.js';
 // // utils
 // export type {AsyncStatus} from './utils/async.js';
 // export {wait, wrap} from './utils/async.js';
-// export type {SpawnedProcess, SpawnResult} from './utils/process.js';
+// export type {Spawned_Process, Spawn_Result} from './utils/process.js';
 // export type {Lazy} from './utils/function.js';
-// export type {ErrorClass} from './utils/error.js';
 // export {UnreachableError} from './utils/error.js';
-// export {last, toArray, EMPTY_ARRAY} from './utils/array.js';
-// export type {Log, LoggerState} from './utils/log.js';
+// export {last, to_array, EMPTY_ARRAY} from './utils/array.js';
+// export type {Log, Logger_State} from './utils/log.js';
 // export {
-// 	LogLevel,
+// 	Log_Level,
 // 	Logger,
-// 	SystemLogger,
-// 	DevLogger,
+// 	System_Logger,
+// 	Dev_Logger,
 // 	ENV_LOG_LEVEL,
 // 	DEFAULT_LOG_LEVEL,
-// 	configureLogLevel,
-// 	printLogLabel,
+// 	configureLog_Level,
+// 	print_log_label,
 // } from './utils/log.js';
-// export {Timings, createStopwatch} from './utils/time.js';
+// export {Timings, create_stopwatch} from './utils/time.js';
 // export type {Stopwatch} from './utils/time.js';
