26f78803b1a490fbe7980c19832e5c2f6879e4ff
Ryan Atkinson <mail@ryanatkn.com>
2021-06-05 08:07:31 -0600

    wip

---
+++ src/lib/Link_Path.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	export let path: string;
+
+	// TODO
+	const to_parts = (path: string): string => path.split('/');
+
+	type Segment_Type = 'space' | 'spacer';
+	interface Segment {
+		type: Segment_Type;
+		content: string;
+	}
+	const segments_cache = new Map<string, Segment[]>();
+	const to_segments = (path: string): Segment[] => {
+		// TODO normalize path?
+		if (segments_cache.has(path)) return segments_cache.get(path)!;
+		// ..
+		let segments: Segment[] = [];
+		for (const part of to_parts(path)) {
+		}
+	};
+
+	$: segments = to_segments(path);
+</script>
+
+{#each segments as segment (segment)}
+	<a href="segment">{segment}</a>
+{/each}
