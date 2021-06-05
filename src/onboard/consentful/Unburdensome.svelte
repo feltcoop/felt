9c60cc17ceabbc05b641fc8fa3a55362e05bca81
Ryan Atkinson <mail@ryanatkn.com>
2021-06-05 10:05:34 -0600

    add content for consentful unburdensome

--- src/onboard/consentful/Unburdensome.svelte
+++ src/onboard/consentful/Unburdensome.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
 	import type {Onboard_Data} from '../onboard';
+	import Markup from '$lib/Markup.svelte';
 
 	export let data: Onboard_Data;
 	export let done: () => void;
 	export let back: () => void;
 </script>
 
-<button on:click={() => done()}> →</button>
+<Markup>
+	<h2>welcome to Felt.dev!</h2>
+</Markup>
+
+<button on:click={() => done()}>
+	<Markup>let's go →</Markup>
+</button>
