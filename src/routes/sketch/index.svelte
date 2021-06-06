3697b8ccacb7001ccc3892a93872111c6b0f0e94
Ryan Atkinson <mail@ryanatkn.com>
2021-06-05 19:58:04 -0600

    add sketch ids

--- src/routes/sketch/index.svelte
+++ src/routes/sketch/index.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import Markup from '$lib/Markup.svelte';
+	import {to_sketch_id} from '../../sketch/sketch';
 	import type {Sketch} from '../../sketch/sketch';
 
-	const sketches: Sketch[] = [{name: 'onboard'}];
+	const sketches: Sketch[] = ['onboard'].map((name) => ({name, id: to_sketch_id(name)}));
 </script>
 
 <nav>
@@ -11,7 +12,7 @@
 			<Markup>
 				<li>
 					<h2>
-						<a href="/sketch/{sketch.name}">{sketch.name}</a>
+						<a href={sketch.id}>{sketch.name}</a>
 					</h2>
 				</li>
 			</Markup>
