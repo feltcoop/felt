d16f8fb24a1490eaa2b7a9b18ec2c95bf2dd35ee
Ryan Atkinson <mail@ryanatkn.com>
2021-06-08 01:45:51 -0600

    update terminal color api

--- src/util/terminal.ts
+++ src/util/terminal.ts
@@ -1,10 +1,29 @@
 import {red, yellow, green, cyan, blue, magenta} from 'kleur/colors';
 
-export * from 'kleur/colors';
+export {
+	bold,
+	gray,
+	black,
+	white,
+	red,
+	yellow,
+	green,
+	cyan,
+	blue,
+	magenta,
+	bgRed as bg_red,
+	bgYellow as bg_yellow,
+	bgGreen as bg_green,
+	bgCyan as bg_cyan,
+	bgBlue as bg_blue,
+	bgMagenta as bg_magenta,
+	bgBlack as bg_black,
+	bgWhite as bg_white,
+} from 'kleur/colors';
 
-const rainbow_colors = [red, yellow, green, cyan, blue, magenta];
+const rainbowColors = [red, yellow, green, cyan, blue, magenta];
 
 export const rainbow = (str: string): string =>
 	Array.from(str)
-		.map((char, i) => rainbow_colors[i % rainbow_colors.length](char))
+		.map((char, i) => rainbowColors[i % rainbowColors.length](char))
 		.join('');
