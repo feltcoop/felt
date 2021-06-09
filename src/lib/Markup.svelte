0dd1ddc6b8c484e91054c90ad3fbdb8c1ecddf39
Ryan Atkinson <mail@ryanatkn.com>
2021-06-09 13:40:07 -0600

    styling tweaks

--- src/lib/Markup.svelte
+++ src/lib/Markup.svelte
@@ -11,6 +11,7 @@
 		align-items: stretch;
 	}
 	.markup :global(ul) {
+		padding: var(--spacing_rg);
 		padding-left: var(--spacing_xl);
 		list-style: disc;
 	}
@@ -18,7 +19,7 @@
 		display: list-item;
 	}
 	.markup :global(p) {
-		padding: var(--spacing_lg) 0;
+		padding: var(--spacing_rg) 0;
 	}
 	.markup :global(h1) {
 		padding-bottom: var(--spacing_lg);
