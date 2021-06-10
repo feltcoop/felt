fd077b671702f55a6a50089600572e322762c2b6
Ryan Atkinson <mail@ryanatkn.com>
2021-06-10 08:50:38 -0600

    revertible content

--- src/lib/onboard/unconsentful/Revertible.svelte
+++ src/lib/onboard/unconsentful/Revertible.svelte
@@ -12,17 +12,23 @@
 		👍 Great! We'll never bother you about those things again unless our lawyers make us.
 	</blockquote>
 	<p>
-		Just checking, you accept all of that stuff, right? Our lawyers are making us ask, you know how
-		lawyers are 🙄
+		If you change your mind you can delete you account <a
+			href="/account"
+			on:click|preventDefault={() =>
+				alert('Thank you for calling support. Please hold until you give up.')}
+			>by calling support</a
+		>.
 	</p>
-	<button on:click={() => done()} />
+	<button on:click={() => done()}>
+		<Markup><span class="text" /></Markup>
+	</button>
 </Markup>
 
 <style>
-	button::after {
-		content: 'I accept →';
+	.text::after {
+		content: 'continue →';
 	}
-	button:hover::after {
-		content: 'What choice do I have?';
+	button:hover .text::after {
+		content: 'do I have a choice?';
 	}
 </style>
